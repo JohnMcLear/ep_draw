5cfaa390c63b3f4515e6815515fc74c43d1c6b63
Chaitanya Raju <yourchaitu@gmail.com>
2020-08-22 22:33:29 +0530

    Fix Hand scroll in mobile

--- static/js/draw.js
+++ static/js/draw.js
@@ -66,11 +66,13 @@ function showdraw(){
       clearTimeout($(this).data('timeout'));
       $("#draw").animate({"width":"100%", "height": "100%"});;
       $("#drawEmbed").animate({"width":"100%", "height": "100%"});;
+      $("#drawEmbed").removeAttr("scrolling");
       clientVars.ep_draw.fullscreen = true;
     }, function(){
       var t = setTimeout(function() { // Dont zoom out right away, wait a while
         $("#draw").animate({"width":"200px", "height": "200px"});;
         clientVars.ep_draw.fullscreen = false;
+        $("#drawEmbed").attr("scrolling", "no");
       }, 500);
       $(this).data('timeout', t);
     });
