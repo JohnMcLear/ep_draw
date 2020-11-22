4a6d37884476c29c9f1ed25a61938011e0aa054e
John McLear <john@mclear.co.uk>
2020-11-22 16:21:16 +0000

    autofixes from Etherpad checkPlugins.js

--- draw.js
+++ draw.js
@@ -1,68 +1,67 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-var settings = require('ep_etherpad-lite/node/utils/Settings');
-var drawString = "";
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+const drawString = '';
 
 exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require("ep_draw/templates/editbarButtons.ejs");
+  args.content += eejs.require('ep_draw/templates/editbarButtons.ejs');
   return cb();
-}
+};
 
-exports.clientVars = function(hook, context, callback){
-  var draw = {};
+exports.clientVars = function (hook, context, callback) {
+  const draw = {};
 
   try {
-    if (settings.ep_draw.host){
+    if (settings.ep_draw.host) {
       draw.host = settings.ep_draw.host;
-    }else{
+    } else {
       console.warn("ep_draw.host NOT SET in settings.json.  The requirement is the host of the etherdraw service IE draw.etherpad.org, copy/paste value to settings.json --  'ep_draw' { 'host': 'your.etherdrawhost.com'}");
-      draw.host = "draw.etherpad.org";
+      draw.host = 'draw.etherpad.org';
     }
-  } catch (e){
+  } catch (e) {
     console.warn("ep_draw.host NOT SET in settings.json.  The requirement is the host of the etherdraw service IE draw.etherpad.org, copy/paste value to settings.json --  'ep_draw' { 'host': 'your.etherdrawhost.com'}");
-    draw.host = "draw.etherpad.org";
+    draw.host = 'draw.etherpad.org';
   }
 
   try {
-    if(settings.ep_draw.onByDefault){
+    if (settings.ep_draw.onByDefault) {
       draw.onByDefault = true;
-    }else{
+    } else {
       draw.onByDefault = false;
     }
-  } catch (e){
+  } catch (e) {
     draw.onByDefault = false;
   }
 
   try {
-    if(settings.ep_draw.icon){
+    if (settings.ep_draw.icon) {
       draw.icon = settings.ep_draw.icon;
-    }else{
-      draw.icon = "../static/plugins/ep_draw/static/img/icon.png";
+    } else {
+      draw.icon = '../static/plugins/ep_draw/static/img/icon.png';
     }
-  } catch (e){
-    draw.icon = "../static/plugins/ep_draw/static/img/icon.png";
+  } catch (e) {
+    draw.icon = '../static/plugins/ep_draw/static/img/icon.png';
   }
 
   try {
-    if(settings.ep_draw.position){
+    if (settings.ep_draw.position) {
       draw.position = settings.ep_draw.position;
-    }else{
-      draw.position = "left";
+    } else {
+      draw.position = 'left';
     }
-  } catch (e){
-    draw.position = "right";
+  } catch (e) {
+    draw.position = 'right';
   }
 
   try {
-    if(settings.ep_draw.autoFullscreen){
+    if (settings.ep_draw.autoFullscreen) {
       draw.autoFullscreen = true;
-    }else{
+    } else {
       draw.autoFullscreen = false;
     }
-  } catch (e){
+  } catch (e) {
     draw.autoFullscreen = false;
   }
 
 
-  return callback( { "ep_draw": draw } );
+  return callback({ep_draw: draw});
 };
-
--- static/js/draw.js
+++ static/js/draw.js
@@ -1,29 +1,28 @@
-var postAceInit = function(hook, context){
-  var draw = clientVars.ep_draw;
-  if(draw){
-    if(draw.onByDefault){ // Setup testing else poop out
-      if(draw.onByDefault === 'true'){
+var postAceInit = function (hook, context) {
+  const draw = clientVars.ep_draw;
+  if (draw) {
+    if (draw.onByDefault) { // Setup testing else poop out
+      if (draw.onByDefault === 'true') {
         enabledraw();
         showdraw();
       }
-    }
-    else{
-      $("#draw").hide();
+    } else {
+      $('#draw').hide();
       clientVars.ep_draw.enabled = false;
       // we don't draw it by default
     }
 
-    $('.toggle_draw').click(function() {
+    $('.toggle_draw').click(() => {
       toggledraw();
     });
   }
 
   try {
-    if (clientVars.ep_draw.icon){
-      $('.draw_icon').css("background-image", 'url('+clientVars.ep_draw.icon+')');
+    if (clientVars.ep_draw.icon) {
+      $('.draw_icon').css('background-image', `url(${clientVars.ep_draw.icon})`);
       $('.draw_icon').css({
-        height: "16px",
-        width: "16px"
+        height: '16px',
+        width: '16px',
       });
     }
   } catch (e) {
@@ -31,85 +30,83 @@ var postAceInit = function(hook, context){
   }
 
   try {
-    if (clientVars.ep_draw.position){
-      if(clientVars.ep_draw.position === "right"){
-        $('.draw').parent().prependTo(".menu_right");
+    if (clientVars.ep_draw.position) {
+      if (clientVars.ep_draw.position === 'right') {
+        $('.draw').parent().prependTo('.menu_right');
       }
-    } 
+    }
   } catch (e) {
 
   }
+};
 
-}
+function enabledraw() {
+  const authorName = 'Testing';
+  const authorColor = $('#myswatch').css('background-color');
+  const draw_host = clientVars.ep_draw.host;
 
-function enabledraw(){
-  var authorName = 'Testing';
-  var authorColor = $('#myswatch').css('background-color');
-  var draw_host = clientVars.ep_draw.host;
+  const padID = clientVars.padId;
 
-  var padID = clientVars.padId;
-
-  if($("#draw").length === 0){ // If it's not available already then draw it
-    $("#editorcontainer").append("<div id=draw><iframe id='drawEmbed' src='//"+draw_host+"/d/"+padID+"?authorName="+authorName+"&authorColor="+authorColor+"' width='100%' height='100%' style='border:none' frameborder='0' scrolling='no'></iframe></div>");
+  if ($('#draw').length === 0) { // If it's not available already then draw it
+    $('#editorcontainer').append(`<div id=draw><iframe id='drawEmbed' src='//${draw_host}/d/${padID}?authorName=${authorName}&authorColor=${authorColor}' width='100%' height='100%' style='border:none' frameborder='0' scrolling='no'></iframe></div>`);
   }
   clientVars.ep_draw.enabled = true;
   showdraw();
 }
 
-function showdraw(){
-  $("#draw").css({"z-index":"999999", "position":"absolute", "top":"0px", "right":"0px", "height":"200px", "width":"200px", "border":"1px solid #ccc"}).show();
-  $("#drawEmbed").show().css({"overflow":"hidden"});
-  if(clientVars.ep_draw.enabled !== true){
+function showdraw() {
+  $('#draw').css({'z-index': '999999', 'position': 'absolute', 'top': '0px', 'right': '0px', 'height': '200px', 'width': '200px', 'border': '1px solid #ccc'}).show();
+  $('#drawEmbed').show().css({overflow: 'hidden'});
+  if (clientVars.ep_draw.enabled !== true) {
     enabledraw();
 
-    $("#draw").hover(function(){
+    $('#draw').hover(function () {
       clearTimeout($(this).data('timeout'));
-      $("#draw").animate({"width":"100%", "height": "100%"});;
-      $("#drawEmbed").animate({"width":"100%", "height": "100%"});;
-      $("#drawEmbed").removeAttr("scrolling");
+      $('#draw').animate({width: '100%', height: '100%'});
+      $('#drawEmbed').animate({width: '100%', height: '100%'});
+      $('#drawEmbed').removeAttr('scrolling');
       clientVars.ep_draw.fullscreen = true;
-    }, function(){
-      var t = setTimeout(function() { // Dont zoom out right away, wait a while
-        $("#draw").animate({"width":"200px", "height": "200px"});;
+    }, function () {
+      const t = setTimeout(() => { // Dont zoom out right away, wait a while
+        $('#draw').animate({width: '200px', height: '200px'});
         clientVars.ep_draw.fullscreen = false;
-        $("#drawEmbed").attr("scrolling", "no");
+        $('#drawEmbed').attr('scrolling', 'no');
       }, 500);
       $(this).data('timeout', t);
     });
-
   }
   clientVars.ep_draw.visible = true;
 }
 
-function hidedraw(){
-  $("#draw").hide();
+function hidedraw() {
+  $('#draw').hide();
   clientVars.ep_draw.fullscreen = false;
   clientVars.ep_draw.visible = false;
 }
 
-function toggledraw(){
-  if(clientVars.ep_draw.visible === true && clientVars.ep_draw.fullscreen){
+function toggledraw() {
+  if (clientVars.ep_draw.visible === true && clientVars.ep_draw.fullscreen) {
     hidedraw();
     return;
   }
 
-  if(!clientVars.ep_draw.visible){
+  if (!clientVars.ep_draw.visible) {
     showdraw();
-    if(clientVars.ep_draw.autoFullscreen){
+    if (clientVars.ep_draw.autoFullscreen) {
       fullScreenDraw();
     }
     return;
   }
-  if(clientVars.ep_draw.visible === true && !clientVars.ep_draw.fullscreen){
+  if (clientVars.ep_draw.visible === true && !clientVars.ep_draw.fullscreen) {
     fullScreenDraw();
     return;
   }
 }
 
-function fullScreenDraw(){
+function fullScreenDraw() {
   clientVars.ep_draw.fullscreen = true;
-  $("#draw").animate({"width":"100%", "height": "100%"});;
-  $("#drawEmbed").animate({"width":"100%", "height": "100%"});;
+  $('#draw').animate({width: '100%', height: '100%'});
+  $('#drawEmbed').animate({width: '100%', height: '100%'});
 }
 
 exports.postAceInit = postAceInit;
